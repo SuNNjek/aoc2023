9ceaff0e145076e5bc7aa9e8c4386c7c0d614ad6
Sunner <sunnerlp@gmail.com>
2023-12-06 21:11:51 +0000

    Day 6 Part 2
    Finally a really nice day again, got to brush up on my high school maths again ^^

--- src/bin/06.rs
+++ src/bin/06.rs
@@ -4,54 +4,54 @@ use std::iter::zip;
 use itertools::Itertools;
 
 struct Race {
-    time: u32,
-    record: u32,
+    time: u64,
+    record: u64,
 }
 
 impl Race {
     #[allow(dead_code)]
-    fn get_distance(&self, hold_time: i32) -> i32 {
-        -(hold_time * hold_time) + (self.time as i32) * hold_time
+    fn get_distance(&self, hold_time: i64) -> i64 {
+        -(hold_time * hold_time) + (self.time as i64) * hold_time
     }
 
     #[allow(dead_code)]
-    fn get_optimal_hold_time(&self) -> u32 {
+    fn get_optimal_hold_time(&self) -> u64 {
         self.time / 2
     }
 
-    fn beat_record_lower_bound(&self) -> u32 {
-        let ftime = self.time as f32;
-        let frec = self.record as f32;
+    fn beat_record_lower_bound(&self) -> u64 {
+        let ftime = self.time as f64;
+        let frec = self.record as f64;
 
         // Good old p-q formula. Takes me back, man ^^
-        let bound = (ftime as f32 / 2.0) - ((ftime * ftime) / 4.0 - frec).sqrt();
+        let bound = (ftime / 2.0) - ((ftime * ftime) / 4.0 - frec).sqrt();
 
         // Sneaky edge case: We need to beat the time, so if the lower bound lands on a whole number
         // we need to add 1 to it so we actually beat it.
         if bound.fract() == 0.0 {
-            (bound as u32) + 1
+            (bound as u64) + 1
         } else {
-            bound.ceil() as u32
+            bound.ceil() as u64
         }
     }
 
-    fn beat_record_upper_bound(&self) -> u32 {
-        let ftime = self.time as f32;
-        let frec = self.record as f32;
+    fn beat_record_upper_bound(&self) -> u64 {
+        let ftime = self.time as f64;
+        let frec = self.record as f64;
 
-        let bound = (ftime as f32 / 2.0) + ((ftime * ftime) / 4.0 - frec).sqrt();
+        let bound = (ftime / 2.0) + ((ftime * ftime) / 4.0 - frec).sqrt();
 
         // Sneaky edge case: We need to beat the time, so if the upper bound lands on a whole number
         // we need to subtract 1 from it so we actually beat it.
         if bound.fract() == 0.0 {
-            (bound as u32) - 1
+            (bound as u64) - 1
         } else {
-            bound.floor() as u32
+            bound.floor() as u64
         }
     }
 }
 
-fn get_numbers(line: &str) -> Option<Vec<u32>> {
+fn get_numbers(line: &str) -> Option<Vec<u64>> {
     let (_, nums) = line.split_once(':')?;
     
     Some(
@@ -59,6 +59,15 @@ fn get_numbers(line: &str) -> Option<Vec<u32>> {
             .filter_map(|n| n.parse().ok())
             .collect()
     )
+}
+
+fn get_number(line: &str) -> Option<u64> {
+    let (_, nums) = line.split_once(':')?;
+
+    nums.split_whitespace()
+        .collect::<String>()
+        .parse()
+        .ok()
 }
 
 fn parse_input(input: &str) -> Option<Vec<Race>> {
@@ -76,7 +85,15 @@ fn parse_input(input: &str) -> Option<Vec<Race>> {
     )
 }
 
-pub fn part_one(input: &str) -> Option<u32> {
+fn parse_single_race(input: &str) -> Option<Race> {
+    let (time, record) = input.lines()
+        .filter_map(get_number)
+        .collect_tuple()?;
+
+    Some(Race { time, record })
+}
+
+pub fn part_one(input: &str) -> Option<u64> {
     let races = parse_input(input)?;
 
     Some(
@@ -91,8 +108,13 @@ pub fn part_one(input: &str) -> Option<u32> {
     )
 }
 
-pub fn part_two(#[allow(unused_variables)] input: &str) -> Option<u32> {
-    None
+pub fn part_two(input: &str) -> Option<u64> {
+    let race = parse_single_race(input)?;
+
+    let lower_bound = race.beat_record_lower_bound();
+    let upper_bound = race.beat_record_upper_bound();
+
+    Some(upper_bound - lower_bound + 1)
 }
 
 #[cfg(test)]
@@ -108,6 +130,6 @@ mod tests {
     #[test]
     fn test_part_two() {
         let result = part_two(&advent_of_code::template::read_file("examples", DAY));
-        assert_eq!(result, None);
+        assert_eq!(result, Some(71503));
     }
 }
